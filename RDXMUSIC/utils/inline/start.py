f12cc22a335c399e52b248ce3cb014874496e023
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-20 01:16:04 +0530

    Update start.py

--- RDXMUSIC/utils/inline/start.py
+++ RDXMUSIC/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", callback_data="mplus HELP_SP"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
