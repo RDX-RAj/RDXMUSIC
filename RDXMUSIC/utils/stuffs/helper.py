ed5be08a498701f8fc1504d9017e6173a766fe69
˹ꝛᴅꭙ ꝛᴀj˼ <159524633+RDX-RAj@users.noreply.github.com>
2024-02-23 00:23:04 +0530

    Update helper.py

--- RDXMUSIC/utils/stuffs/helper.py
+++ RDXMUSIC/utils/stuffs/helper.py
@@ -236,6 +236,32 @@ Example:
 ◌ /q r : ᴄʀᴇᴀᴛᴇ ᴀ ǫᴜᴏᴛᴇ ғʀᴏᴍ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʀᴇᴘʟʏ
 '''
 
+   HELP_RDXS = ''' ꝛᴅꭙ ѕᴜᴘᴘᴏʀᴛ
+
+✪ ᴛʜᴇ ꝛᴅꭙ ѕᴜᴘᴘᴏʀᴛ ᴍᴇɴᴜ ✪
+
+๏ ᴠɪᴘ ғʀɪᴇɴᴅᴢ ᴄʟᴜʙ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+RObRa7kXPIJmMjU1)
+
+๏ ꝛᴅꭙ ᴜᴘᴅᴀᴛᴇs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+m4oVCt2zFhYyMTdl)
+
+๏ ꝛᴅꭙ sᴛᴜᴅʏs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+PtOLQT04ocMzOTJl)
+
+๏ ꝛᴅꭙ ʙᴏᴛs ʟɪsᴛ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+KRt7ApiJ5Ek2ZjY1)
+
+๏ ꝛᴅꭙ ʜᴏᴛ ᴅᴇᴀʟs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+BD8IVag4UpU3ODI1)
+
+๏ ᴀʙᴏᴜᴛ ꝛᴅꭙ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+okCO84m2hZxjYTE9)
+
+๏ ꝛᴅꭙ sᴜᴘᴘᴏʀᴛs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+pAWm7GPhFYliOGI1)
+
+๏ sᴛᴜᴅʏ ᴄʜᴀɴɴᴇʟ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+A8Dk4aNJet44ZDA1)
+
+๏ ꝛᴅꭙ ʏᴏᴜᴛᴜʙᴇ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw)
+
+๏ ɪɴsᴛᴀɢʀᴀᴍ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://www.instagram.com/vdjrajofficial?igsh=ZDExYjZkNGI0OA==)
+
+'''
+
     
     fullpromote = {
     'can_change_info': True,
