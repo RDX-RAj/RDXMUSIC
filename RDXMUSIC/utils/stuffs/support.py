869e67f3aa9ed058eb862ca11f0d99d10c2a70db
˹ꝛᴅꭙ ꝛᴀj˼ <159524633+RDX-RAj@users.noreply.github.com>
2024-02-23 00:06:21 +0530

    Update support.py

--- RDXMUSIC/utils/stuffs/support.py
+++ RDXMUSIC/utils/stuffs/support.py
@@ -3,8 +3,29 @@ class Helper(object):
     HELP_S = '''ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ғᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴩ.
 ᴀsᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛs ᴀᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ
 
-ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /'''
+ᴀʟʟ ʙᴜᴛᴛᴏɴs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : ᴄʟɪᴄᴋ'''
     HELP_RDXS = ''' ˹ꝛᴅꭙ ѕᴜᴘᴘᴏʀᴛ
 
-ᴛʜᴇ ꝛᴅꭙ ѕᴜᴘᴘᴏʀᴛ ᴍᴇɴᴜ:
-[ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+RObRa7kXPIJmMjU1)
+✪ ᴛʜᴇ ꝛᴅꭙ ѕᴜᴘᴘᴏʀᴛ ᴍᴇɴᴜ ✪
+
+๏ ᴠɪᴘ ғʀɪᴇɴᴅᴢ ᴄʟᴜʙ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+RObRa7kXPIJmMjU1)
+
+๏ ꝛᴅꭙ ᴜᴘᴅᴀᴛᴇs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+m4oVCt2zFhYyMTdl)
+
+๏ ꝛᴅꭙ sᴛᴜᴅʏs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+PtOLQT04ocMzOTJl)
+
+๏ ꝛᴅꭙ ʙᴏᴛs ʟɪsᴛ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+KRt7ApiJ5Ek2ZjY1)
+
+๏ ꝛᴅꭙ ʜᴏᴛ ᴅᴇᴀʟs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+BD8IVag4UpU3ODI1)
+
+๏ ᴀʙᴏᴜᴛ ꝛᴅꭙ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+okCO84m2hZxjYTE9)
+
+๏ ꝛᴅꭙ sᴜᴘᴘᴏʀᴛs ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+pAWm7GPhFYliOGI1)
+
+๏ sᴛᴜᴅʏ ᴄʜᴀɴɴᴇʟ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/+A8Dk4aNJet44ZDA1)
+
+๏ ꝛᴅꭙ ʏᴏᴜᴛᴜʙᴇ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw)
+
+๏ ɪɴsᴛᴀɢʀᴀᴍ ➠ [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://www.instagram.com/vdjrajofficial?igsh=ZDExYjZkNGI0OA==)
+
+'''
