0771b0aa36cb9bec0c6cf4f7ea19af891fa12926
˹ꝛᴅꭙ ꝛᴀj˼ <159524633+RDX-RAj@users.noreply.github.com>
2024-02-23 07:49:04 +0530

    Update start.py

--- RDXMUSIC/utils/inline/start.py
+++ RDXMUSIC/utils/inline/start.py
@@ -39,12 +39,7 @@ def private_panel(_):
             InlineKeyboardButton(text="˹sᴜᴍᴍᴏɴ ᴍє ʙᴀʙʏ˼", url=f"https://t.me/{app.username}?startgroup=true",),],
         [
             InlineKeyboardButton(text="˹๏ᴡɴєʀ˼", user_id="1777270311"),
-            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", callback_data="mplus HELP_For_u"),
-        ],
-        [
-            InlineKeyboardButton(text="˹❀˼", url="https://t.me/+okCO84m2hZxjYTE9"),
-            InlineKeyboardButton(text="˹˹ʏᴛ˼", url="https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw"),
-            InlineKeyboardButton(text="˹❀˼", url="https://t.me/+pAWm7GPhFYliOGI1"),
+            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛєѕ˼", url="https://t.me/+m4oVCt2zFhYyMTdl"),
         ],
     ]
     return buttons
