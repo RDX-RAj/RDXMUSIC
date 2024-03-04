23c93be5b38f25c9343c76808eefb7b0d3f5a9ba
˹ꝛᴅꭙ ꝛᴀj˼ <159524633+RDX-RAj@users.noreply.github.com>
2024-03-04 08:27:49 +0530

    Update extras.py

--- RDXMUSIC/utils/inline/extras.py
+++ RDXMUSIC/utils/inline/extras.py
@@ -32,9 +32,15 @@ def supp_markup(_):
         [
             [
                 InlineKeyboardButton(
-                    text=_["S_B_9"],
-                    url=SUPPORT_CHAT,
+                    text="˹ᴍᴜsɪᴄ˼",
+                    callback_data="help_callback"
                 ),
+                InlineKeyboardButton(
+                    text="˹ᴍᴀɴᴀɢᴇᴍᴇɴᴛ˼",
+                    callback_data="mbot_cb"),][
+                InlineKeyboardButton(
+                    text="˹sᴘᴇᴄɪᴀʟ ᴛᴀɢᴇʀ˼",
+                    k_data="mplus HELP_TagAll"),
             ]
         ]
     )
